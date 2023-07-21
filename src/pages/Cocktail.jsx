ec5cc1ec37d22fbdd1adc835da98e94bbeb85f81
Salimata Azzolini <100932041+SalimataAzzolini@users.noreply.github.com>
2023-07-21 10:47:57 +0200

    Update Cocktail.jsx

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -4,7 +4,6 @@ import { useParams } from 'react-router-dom';
 const Cocktail = ({cocktail}) => {
     console.log(cocktail);
     let {uid} = useParams();
-   /*  console.log(uid); */
     return (
         <div>
             <h3> Le cocktail </h3>
